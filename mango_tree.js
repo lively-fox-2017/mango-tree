7d4c9afaa0379786d73bfb29a2365a4e7a5bd5e8
Ian Max <ianmax1983@gmail.com>
2017-09-25 22:28:57 +0700

    Fruit

--- mango_tree.js
+++ mango_tree.js
@@ -1,21 +1,43 @@
-"use strict"
+'use strict'
 
 // release 0
 
 class MangoTree {
 
   // Initialize a new MangoTree
-  constructor() {
+  constructor(age, height, fruits, health) {
+    this._age = age;
+    this._height = height;
+    this._maxFruit = Math.ceil(Math.random() * 15);
+    this._fruits = fruits;
+    this._produce = 0;
+    this._health = true;
+    this._addHeight = Math.floor((Math.random() * (5 - 1)) + 10) / 10;
+    this.mature = 3;
+    this.maxAge = 20;
+    this.maxGrow = 15;
+    this.maxHeight = 6.5;
+
   }
 
   getAge() {
+    return this._age;
   }
-  getHeight() {
 
+  getHeight() {
+    if (this._height < this.maxHeight)
+    return this._height;
+    else {
+      return this.maxHeight;
+    }
   }
+
   getFruits() {
+    return this._fruits;
   }
+
   getHealtyStatus() {
+    return this._health;
   }
 
 
@@ -23,6 +45,19 @@ class MangoTree {
 
   // Grow the tree
   grow() {
+    this._age += 1;
+    if (this._age === this.maxAge)
+    this._health = false;
+
+    if (this._age === this.mature)
+    this._mature = true;
+
+    this._height += this._addHeight;
+
+    if (this._age >= this.mature || this._age <= this.maxAge)
+    this._fruits = this._maxFruit;
+
+
   }
 
   // Produce some mangoes
@@ -51,14 +86,22 @@ class Mango {
   *   console.log(`[Year ${tree._age} Report] Height = ${tree._height} | Fruits harvested = ${tree._harvested}`)
   * } while (mangoTree.healthyStatus != false)
   */
+//
+// // Release 1
+// class AppleTree {}
+// class Apple {}
+//
+// // Release 2
+// class FruitTree {}
+// class Fruit {}
+//
+// // Release 3
+// class TreeGrove {}
 
-// Release 1
-class AppleTree {}
-class Apple {}
+let mangga = new MangoTree(1, 1.3, 5, true);
+mangga.grow()
 
-// Release 2
-class FruitTree {}
-class Fruit {}
-
-// Release 3
-class TreeGrove {}
+console.log(mangga.getAge());
+console.log(mangga.getHeight());
+console.log(mangga.getFruits());
+console.log(mangga.getHealtyStatus());

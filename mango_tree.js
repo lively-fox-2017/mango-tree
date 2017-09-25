cd36bbc79634384451cd1499dabe7c4bc3a00fcf
kalibani <kalibani.ka@gmail.com>
2017-09-25 16:03:53 +0700

    commit ke 1 sampe produce mango

--- mango_tree.js
+++ mango_tree.js
@@ -6,16 +6,26 @@ class MangoTree {
 
   // Initialize a new MangoTree
   constructor() {
+    this._age = 0
+    this._height = 0
+    this._harvested = 0
+    this._healthyStatus = true
+    this._stopGrowing = 23
+    this._maxAge = 25
+    this._fruits = []
   }
 
   getAge() {
+    return this._age
   }
   getHeight() {
-
+    return this._height
   }
   getFruits() {
+    return this._harvested
   }
   getHealtyStatus() {
+    return this._healthyStatus
   }
 
 
@@ -23,14 +33,37 @@ class MangoTree {
 
   // Grow the tree
   grow() {
+    this._age++
+    if (this._age <= this._stopGrowing) {
+      let random = Math.floor(Math.random()*2,5)
+      this._height += random
+    }
+    if (this._age == this._maxAge) {
+      this._healthyStatus = false
+    }
+    return this
   }
 
   // Produce some mangoes
   produceMangoes() {
+    let random = Math.floor(Math.random()* this._age)
+    for (var i = 0; i < random; i++) {
+      this._fruits.push(new Mango())
+    }
   }
 
   // Get some fruits
   harvest() {
+    let good = 0
+    let bad = 0
+    for (var i = 0; i < this._fruits.length; i++) {
+      if (this._fruits[i]._quality == 'good') {
+        good++
+      }else {
+        bad++
+      }
+    }
+    this._harvested = this._fruits.length +' (good '+good+' bad '+bad +')'
   }
 
 }
@@ -38,27 +71,32 @@ class MangoTree {
 class Mango {
   // Produce a mango
   constructor() {
+    this._quality = this.randomBuah()
+  }
+  randomBuah(){
+    this._isQuality = ['good','bad']
+    this._quality = this._isQuality[Math.floor(Math.random()*this._isQuality.length)]
+    return this._quality
   }
 }
 
-/**
-  * driver code untuk release 0
-  * let mangoTree = new MangoTree()
-  * do {
-  *   mangoTree.grow();
-  *   mangoTree.produceMangoes();
-  *   mangoTree.harverst();
-  *   console.log(`[Year ${tree._age} Report] Height = ${tree._height} | Fruits harvested = ${tree._harvested}`)
-  * } while (mangoTree.healthyStatus != false)
-  */
+ // driver code untuk release 0
+   let mangoTree = new MangoTree()
+   do {
+     mangoTree.grow();
+     mangoTree.produceMangoes();
+     mangoTree.harvest();
+     console.log(`[Year ${mangoTree._age} Report] Height = ${mangoTree._height} m | Fruits harvested = ${mangoTree._harvested}`)
+   } while (mangoTree._healthyStatus != false)
+
 
 // Release 1
-class AppleTree {}
-class Apple {}
+// class AppleTree {}
+// class Apple {}
 
 // Release 2
-class FruitTree {}
-class Fruit {}
+// class FruitTree {}
+// class Fruit {}
 
 // Release 3
-class TreeGrove {}
+// class TreeGrove {}

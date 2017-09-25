136610baa21937529dcf837384a051526203c4d0
lokiloko <ianw.tkj2@gmail.com>
2017-09-25 16:27:12 +0700

    Release 3, split class into file

---
+++ pear.js
@@ -0,0 +1,7 @@
+"use strict"
+var Fruit = require('./fruit.js');
+class Pear extends Fruit {
+
+}
+
+module.exports = Pear;
--- pear_tree.js
+++ pear_tree.js
@@ -1,15 +1,13 @@
 "use strict"
 var FruitTree = require('./fruit_tree.js');
-// release 0
 
-class MangoTree extends FruitTree {
-  // Produce some mangoes
+class PearTree extends FruitTree {
   constructor(name, currentAge, currentHeight, berbuah, healthyStatus) {
     super(name, currentAge, currentHeight, berbuah, healthyStatus);
-    this._maxAge = 15;
-    this._heightGrow = 1.2;
+    this._maxAge = 20
+    this._heightGrow = 0.8;
   }
+  // Produce some mangoes
 }
-module.exports = MangoTree;
 
-// Release 3
+module.exports = PearTree;
